7cd757c02ee3a632db13701dcfcacd9288249fa4
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2021-12-19 17:01:49 -0500

    Moved onclick funtion to app for light switch

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import TextRepeaterButton from "./components/TextRepeaterButton";
 function App() {
     const [light, setLight] = useState("off");
     const dark = light === "off" ? "dark" : "";
+
+    const switchLight = () => setLight(light === "on" ? "off" : "on");
+
     return (
         <div className={`App ${dark}`}>
             <h1>Fancy Buttons!</h1>
             <section>
                 <AngryButton />
                 <CounterButton />
-                <LightSwitchButton light={light} setLight={setLight} />
+                <LightSwitchButton light={light} switchLight={switchLight} />
                 <TextRepeaterButton />
             </section>
         </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,9 +1,8 @@
 function LightSwitchButton(props) {
-    const { light, setLight } = props;
-    const handleClick = () => setLight(light === "on" ? "off" : "on");
+    const { light, switchLight } = props;
 
     return (
-        <button onClick={handleClick} className='LightSwitchButton'>
+        <button onClick={switchLight} className='LightSwitchButton'>
             {light === "on" && (
                 <span>
                     <i>💡</i> I'm on!
